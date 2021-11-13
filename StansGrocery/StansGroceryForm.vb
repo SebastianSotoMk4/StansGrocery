fbb8db22df69c2a322bcfadb95dc33eca7cfdbb6
Sebastian Soto <thefirstcoolone@gmail.com>
2021-11-12 20:29:31 -0700

    read from file and into an array.

--- StansGrocery/StansGroceryForm.vb
+++ StansGrocery/StansGroceryForm.vb
@@ -9,42 +9,44 @@ Option Strict On
 Option Explicit On
 Public Class StansGroceryForm
     Dim fileName As String = "..\..\Grocery.txt"
+    Dim groceryData(199, 3) As String
+    Dim tempList As New List(Of String)
+
     Sub SampleTxt()
         Dim currentRecord As String
-
         FileOpen(1, "Grocery.txt", OpenMode.Input)
         Do Until EOF(1)
             Input(1, currentRecord)
             ProductListBox.Text = currentRecord
             'Console.WriteLine(currentRecord)
-
         Loop
-
         FileClose(1)
-
-
-
     End Sub
-
+    'sets defaults when loaded or called
     Sub Defaults()
         AisleRadioButton.Checked = True
-
     End Sub
+    'Needs to ask to open a file
     Sub ReadFile()
         'Clear listbox each time called for neatness
         ProductListBox.Items.Clear()
-
-        Dim currentRecord As String
+        Dim currentLine As String
         Dim temp() As String
-
-        FileOpen(1, Me.fileName, OpenMode.Input)
-
-        Do Until EOF(1)
-            Input(1, currentRecord)
-            ProductListBox.Items.Add($"{currentRecord}")
-
-        Loop
-        FileClose(1)
+        Dim itemNo As Integer
+        Try
+            FileOpen(1, Me.fileName, OpenMode.Input)
+            Do Until EOF(1)
+                For lineNumber = 0 To 3
+                    Input(1, currentLine)
+                    Me.groceryData(itemNo, lineNumber) = currentLine
+                    FileClose(1)
+                Next
+                itemNo += 1
+            Loop
+        Catch ex As Exception
+            ' ask to choose a file 
+            MsgBox($"no file ???")
+        End Try
     End Sub
 
     Function SeperateRecords(ByVal record As String) As String
@@ -55,4 +57,5 @@ Public Class StansGroceryForm
         ReadFile()
     End Sub
 End Class
-''Could not find file 'C:\Users\thefi\source\repos\StansGrocery\StansGrocery\.Grocery.txt'.'
+' How do i Open a fileDialog?? previous methods are not working.
+' 
